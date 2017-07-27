f45e09a2629ec36d65b84228122295d11a474cc3
Tyrone Trevorrow <tyrone.trevorrow@marcopolee.com>
2017-07-27 15:22:26 +1000

    feat: buddybuild hipchat webhook

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const request = require("request");
-const discordUrl = process.env["DISCORD_WEBHOOK"];
+const bugsnagWebhook = process.env["BUGSNAG_WEBHOOK"];
+const buddybuildWebhook = process.env["BUDDYBUILD_WEBHOOK"];
+
+function assertEnvVariable(variable) {
+    if (!process.env[variable]) {
+        console.error(`fatal: missing environment variable: ${variable}`);
+        process.exit(1);
+    }
+}
+
+assertEnvVariable("BUGSNAG_WEBHOOK");
+assertEnvVariable("BUDDYBUILD_WEBHOOK")
 
 app.use(bodyParser.json());
 
+// Bugsnag
 app.post('/webhooks/bugsnag', (req, res) => {
-    console.log(req.body);
     var bug = req.body;
     try {
         var fields = [];
@@ -38,7 +49,7 @@ app.post('/webhooks/bugsnag', (req, res) => {
         }
 
         request({
-            url: discordUrl,
+            url: bugsnagWebhook,
             method: "POST",
             body: message,
             json: true
@@ -53,6 +64,98 @@ app.post('/webhooks/bugsnag', (req, res) => {
         res.status(500);
         res.end();
     }
+});
+
+// Hipchat supports: yellow, green, red, purple, gray, random.
+function convertHipchatColorToDiscord(color) {
+    const COLORS = {
+        yellow: 0xF1C40F,
+        green: 0x2ECC71,
+        red: 0xE74C3C,
+        purple: 0x9B59B6,
+        gray: 0x95A5A6,
+        grey: 0x95A5A6
+    }
+    if (color.toLowerCase() === 'random') {
+        return Math.floor(Math.random() * (0xFFFFFF + 1));
+    }
+    const resolvedColor = COLORS[color.toLowerCase()];
+    if (resolvedColor) {
+        return resolvedColor;
+    } else {
+        throw new Error("invalid hipchat color encountered")
+    }
+}
+
+function convertHipchatAttrsToDiscordFields(attributes) {
+    let fields = [];
+    let actions = [];
+    for (const attr of attributes) {
+        let field = {};
+        if (!attr.label && attr.value.label && attr.value.url) {
+            actions.push(attr);
+            continue;
+        }
+        field.name = attr.label;
+        if (attr.value.url) {
+            field.value = `[${attr.value.label}](${attr.value.url})`;
+        } else {
+            field.value = attr.value.label;
+        }
+        fields.push(field);
+    }
+    if (actions.length > 0) {
+        let field = {};
+        field.name = "Actions";
+        field.value = actions.map((attr) => `[${attr.value.label}](${attr.value.url})`).join(" ");
+        fields.push(field);
+    }
+    return fields;
+}
+
+// Buddybuild
+app.all('/webhooks/buddybuild', (req, res) => {
+    const msg = req.body;
+    console.log(JSON.stringify(msg));
+    try {
+        const color = convertHipchatColorToDiscord(msg.color.toLowerCase());
+        const message = {
+            embeds: [
+                {
+                    title: msg.card.title,
+                    url: msg.card.url,
+                    description: msg.card.description,
+                    color: color,
+                    fields: convertHipchatAttrsToDiscordFields(msg.card.attributes)
+                }
+            ]
+        }
+
+        request({
+            url: buddybuildWebhook,
+            method: "POST",
+            body: message,
+            json: true
+        }, (e,r,b) => {
+            if (e) throw e;
+        });
+
+        res.status(200);
+        res.end();
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+        res.end();
+    }
+});
+
+// Default
+
+app.use((req, res) => {
+    console.log(req.path);
+    console.log(req.body);
+    res.sendStatus(404);
+    res.end();
 });
 
 const port = process.env["PORT"] || 11337
